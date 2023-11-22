5533b265a62c7a71efc7fb8b0521b482e1d29a7c
Morgane Debarge <morganedebarge.05@gmail.com>
2023-11-22 16:21:20 +0100

    ajout page error et router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-import './App.css'
+import "./App.scss";
 
 function App() {
-  
-
   return (
     <>
-      
+      <NavBar />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Accueil />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
