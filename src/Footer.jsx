c111a9bbb315e8565c75828d77adaaf8356b25b8
Alexandre-27 <alexandre24102000@gmail.com>
2023-11-23 17:41:20 +0100

    Footer position at bottom of page

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,10 @@
 function Footer() {
-  return <div className="Footer flex items-center justify-between">
-    <p className="pl-2">TEAM NAVIDAD</p>
-    <p className="pr-2">@copyrigth HACKANOEL tous droits réservés</p>
-    </div>;
+  return (
+    <footer className="Footer flex items-center justify-between">
+      <p className="pl-2">TEAM NAVIDAD</p>
+      <p className="pr-2">@copyrigth HACKANOEL tous droits réservés</p>
+    </footer>
+  );
 }
 
 export default Footer;
