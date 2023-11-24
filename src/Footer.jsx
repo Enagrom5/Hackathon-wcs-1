126e190560f8b6cca7c7c31769f5280323565a46
Alexandre-27 <alexandre24102000@gmail.com>
2023-11-24 02:10:19 +0100

    addition of media queries and other minor changes such as pluses and minuses in product cards replaced by cart icons ect

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="Footer flex items-center justify-between">
+    <footer className="Footer flex items-center justify-between text-[10px] sm:text-[12px] md:text-[15px]">
       <p className="pl-2">TEAM NAVIDAD</p>
       <p className="pr-2">@copyrigth HACKANOEL tous droits réservés</p>
     </footer>
--- src/Modal/login.jsx
+++ src/Modal/login.jsx
@@ -4,17 +4,17 @@ import { IoCloseOutline } from "react-icons/io5";
 function Login({ closeModal }) {
   return (
     <section>
-      <div className="flex justify-center items-center">
-        <div className="w-[40%] bg-gray-300 px-8 pb-12 pt-2 text-center rounded-xl">
-          <h2 className="text-3xl font-bold md:text-5xl">
+      <div className="flex justify-center items-center mt-[9px] md:mt-none">
+        <div className="w-[80%] md:w-[40%] bg-gray-300 px-8 pb-12 pt-2 text-center rounded-xl">
+          <h2 className="md:text-3xl font-bold md:text-5xl">
             <div className="flex justify-end">
               <IoCloseOutline
-                className="text-[#636262] text-2xl translate-x-4 cursor-pointer hover:text-red-500"
+                className="text-[#636262] text-[25px] md:text-2xl translate-x-4 sm:cursor-pointer sm:hover:text-red-500"
                 onClick={() => closeModal(false)}
               />
             </div>
           </h2>
-          <p className="mx-auto my-5 max-w-md text-sm text-[#636262] sm:text-base lg:mb-8">
+          <p className="mx-auto my-5 max-w-md md:text-sm text-[#636262] sm:text-base lg:mb-8">
             Lorem ipsum dolor sit amet consectetur adipiscing elit ut
             aliquam,purus sit amet luctus magna fringilla urna
           </p>
@@ -28,22 +28,22 @@ function Login({ closeModal }) {
                 alt=""
                 className="mr-4 inline-block"
               />
-              <p className="text-sm sm:text-base">Sign up with Google</p>
+              <p className="md:text-sm sm:text-base">Sign up with Google</p>
             </p>
-            <div className="mb-14 mt-14 flex w-full justify-around">
+            <div className="mb-8 md:mb-14 mt-8 md:mt-14 flex w-full justify-around">
               <img
                 src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a948ef4e6cf94_Line%203.svg"
                 alt=""
                 className="inline-block"
               />
-              <p className="text-sm text-[#636262]">or sign up with email</p>
+              <p className="md:text-sm text-[#636262]">or sign up with email</p>
               <img
                 src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a948ef4e6cf94_Line%203.svg"
                 alt=""
                 className="inline-block"
               />
             </div>
-            <div className="mx-auto mb-4 max-w-[400px] pb-4">
+            <div className="mx-auto mb-1 md:mb-4 max-w-[400px] pb-4">
               <form name="wf-form-password" method="get">
                 <div className="relative">
                   <img
@@ -51,12 +51,12 @@ function Login({ closeModal }) {
                     src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a9455fae6cf89_EnvelopeSimple.svg"
                     className="absolute left-[5%] top-[46%] inline-block"
                   />
-                  <p className="peer-invalid:visible text-pink-600 text-sm text-left">
+                  <p className="peer-invalid:visible text-pink-600 md:text-sm text-left">
                     Veuillez saisir une adresse mail valide.
                   </p>
                   <input
                     type="email"
-                    className="peer mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-14 text-sm text-[#333333] focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                    className="peer mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-14 md:text-sm text-[#333333] focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
                     placeholder="Email Address"
                     required=""
                   />
@@ -67,12 +67,12 @@ function Login({ closeModal }) {
                     src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a946794e6cf8a_Lock-2.svg"
                     className="absolute left-[5%] top-[46%] inline-block"
                   />
-                  <p className="per-invalid:visible text-pink-600 text-sm text-left">
+                  <p className="per-invalid:visible text-pink-600 md:text-sm text-left">
                     Veuillez saisir un mot de passe valide.
                   </p>
                   <input
                     type="password"
-                    className="per mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-14 text-sm text-[#333333] focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                    className="per mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-14 md:text-sm text-[#333333] focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
                     placeholder="Password (min 8 characters)"
                     required=""
                   />
@@ -80,11 +80,11 @@ function Login({ closeModal }) {
                 <input
                   type="submit"
                   value="Sign Up"
-                  className="mt-4 inline-block w-full cursor-pointer items-center rounded-md bg-purple-heart-500 px-6 py-3 text-center font-semibold text-white"
+                  className="mt-2 md:mt-4 inline-block w-full cursor-pointer items-center rounded-md bg-purple-heart-500 px-6 py-3 text-center font-semibold text-white"
                 />
               </form>
             </div>
-            <p className="text-sm text-[#636262]">
+            <p className="md:text-sm text-[#636262]">
               Already have an account?{" "}
               <p href="#" className="font-bold text-purple-heart-900 ">
                 Login now
